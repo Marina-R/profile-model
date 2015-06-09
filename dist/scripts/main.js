2ab8b2841b9623277344d10edfb3a58dc549ebc1
maryna <luv_this_world@hotmail.com>
2015-06-09 10:43:54 -0500

    homework hard mode

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,99 @@
-var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
-});
+$(document).ready(function() {
 
-var app = new App();
-Backbone.history.start();
+	var user = new UserModel();
+	//in-calss way to make a GET request:
+
+	//user.fetch();
+
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'home': 'profile',
+			'editProfile': 'edit',
+			'edit': 'edit'
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+			// in-class notes: 
+
+			// $('#name').val(user.get('name'));
+			// $('#inputEmail3').val(user.get('email'));
+			// $('#role').val(user.get('role'));
+		}
+	});
+
+	var app = new App();
+	Backbone.history.start();
+	
+	var url = 'https://tiny-pizza-server.herokuapp.com/collections/marina/55760522a73f360300000149';	
+
+	var name = user.get('name'); 
+	var email = user.get('email');
+	var role = user.get('role');
+
+	$.get(url, function(data) {
+		user.set(data);
+		name = data.name;
+		email = data.email;
+		role = data.role;
+	}, 'json');
+
+	$('.editProfile').click(function(e) {
+		app.navigate('edit');
+		$('#name').attr('placeholder', name);
+		$('#inputEmail3').attr('placeholder', email);
+		$('#role').attr('placeholder', role);
+
+	});
+
+	changeProfile(user);
+	user.on('change', changeProfile);
+
+	function changeProfile(thisUser) {
+		$('.userName').html(thisUser.get('name'));
+		$('#job').html(thisUser.get('role'));	
+	}
+
+	$('#editProfileBtn').click(function(e) {
+		e.preventDefault();
+		// in-class notes
+
+/* 
+		var user = new UserModel({
+			name: $('#name').val(),
+			email: $('#inputEmail3').val(),
+			role: $('#role').val(),
+			password: $('#inputPassword3').val()
+		});
+		user.on('change', changeProfile);
+		user.save();
+	});
+*/
+		user.set({
+			name: $('#name').val(),
+			email: $('#inputEmail3').val(),
+			role: $('#role').val(),
+			password: $('#inputPassword3').val()
+		});
+
+		// in-class way to save data on server:
+		//user.save(); ===> making POST or PUT requests depending on if we have a record or not
+
+		$.ajax({
+			url: url,
+			method: 'put',
+			data: {
+				'name': $('#name').val(),
+				'email': $('#inputEmail3').val(),
+				'role': $('#role').val(),
+				'password': $('#inputPassword3').val()
+			}
+		});
+	});
+	
+})
--- dist/scripts/models/user.js
+++ dist/scripts/models/user.js
@@ -1,5 +1,11 @@
 var UserModel = Backbone.Model.extend({
+	//in-class way:
+
+	//urlRoot: 'https://tiny-pizza-server.herokuapp.com/collections/marina/55760522a73f360300000149',
+	//idAttribute: '_id',	===> setting the name of attr id (specifying that the name of id we are going to use is going to be '_id')
+
 	defaults: {
+		//_id: 'jhkhgllgl',
 		name: 'Marcus Doe',
 		email: 'mdoe@gmail.com',
 		role: 'Developer'
